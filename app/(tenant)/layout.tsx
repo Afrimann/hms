b1240566158b5e1b1a85c568362cdcc01239b40b
Afrimann <omotoshopeter737@gmail.com>
2026-05-21 15:22:45 +0100

    configure layout settings for pages

---
+++ app/(tenant)/layout.tsx
@@ -0,0 +1,17 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "MedCloud | Your Hospital in the Cloud",
+  description:
+    "A hospital management system that organzies different departments or units in a hospital and manages their operations distinctively while contributing to the fast and efficient operation in hospitals worldwide",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div>{children}</div>
+  );
+}
